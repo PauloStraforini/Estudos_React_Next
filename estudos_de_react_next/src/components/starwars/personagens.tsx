10179e8e68db0c24a0091121672a394934c974a1
PauloStraforini <paulostraforini@gmail.com>
2024-10-10 16:26:42 -0300

    Refactor
    Refactor: Remove unused constantes/produtos.ts file and update layout/index.tsx and starwars/personagens.tsx components

--- estudos_de_react_next/src/components/starwars/personagens.tsx
+++ estudos_de_react_next/src/components/starwars/personagens.tsx
@@ -1,7 +1,26 @@
 export default function Personagens() {
+
+    async function SimularCamadaAPI() {
+        return new Promise((resolve) => {
+            setTimeout (() => {
+                resolve (1)
+            },  3000)
+        const data = Date.now()
+        while (Date.now() < data + 3000) { }
+        })
+    }
+
+    async function obterPersonagens() {
+        console.log('Início da função obterPersonagens')
+        await SimularCamadaAPI()
+        console.log('Fim da função obterPersonagens')
+    }
+
     return (
         <div className={`flex justify-center items-center h-screen`}>
             <h1 className="text-3xl text-center">Personagens</h1>
+            <button onClick={obterPersonagens} className={`
+                bg-blue-500 p-2`}>obter</button>
         </div>
     )
 } 
