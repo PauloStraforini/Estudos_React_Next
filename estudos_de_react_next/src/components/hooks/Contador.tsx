9b1f61b41b01cf2ab1e69763629e24a04282ec1c
PauloStraforini <paulostraforini@gmail.com>
2024-10-12 10:38:56 -0300

    :computer: Refactor: Remove unused constantes/produtos.ts file and update layout/index.tsx and starwars/personagens.tsx components

---
+++ estudos_de_react_next/src/components/hooks/Contador.tsx
@@ -0,0 +1,39 @@
+import { use, useEffect, useState } from "react";
+
+export default function Contador() {
+    const [delta, setDelta] = useState(1);
+    const [valor, setValor] = useState(0);
+    const  [mult10, setMult10] = useState(0);
+
+    function CalcularValorMult10()  {
+        return valor * 10
+    }
+
+    useEffect(() => {
+       const mult10 = CalcularValorMult10()
+       setMult10 (mult10)
+    }, [valor, CalcularValorMult10])
+
+    // useEffect(() => {
+    //     setMult10(valor * 10)
+    // }, [valor])
+
+    return (
+        <div className="flex flex-col items-center gap-5">
+            <span className="text-5xl">{valor}</span>
+            <span className="text-xl text-blue-300">{mult10}</span>
+            <span className="text-sm text-zinc-500">{delta}</span>
+            <div className="flex gap-5"> 
+                <button className="botao" onClick={() => setValor(valor - delta)}
+                >Incrementar</button>
+                <button className="botao" onClick={() => setValor(valortual => valortual + delta)}
+                >Decrementar</button>
+            </div>
+            <input type="number" value={delta} onChange={e => setDelta
+                (+e.target.value)}
+                className="bg-zinc-900 p-2"
+             />
+    
+        </div>
+    )
+}
---
+++ estudos_de_react_next/src/pages/hooks/contador.tsx
@@ -0,0 +1,9 @@
+import Contador from "@/components/hooks/Contador";
+
+export default function PaginaContador () {
+    return (
+        <div>
+            <Contador />
+        </div>
+    )
+}
